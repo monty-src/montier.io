65f722b715136958e6c74cdf3494cfcea84e1cd3
monty-src <phoebe@montiers-mbp.lan>
2022-09-24 16:20:46 -0400

    resolved style-components issue

--- next.config.js
+++ next.config.js
@@ -2,11 +2,12 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
+  compiler: {
+    styledComponents: true,
+  },
   env: {
     API_BASE_URL: process.env.API_BASE_URL,
-  },
-  presets: ["next/babel"],
-  plugins: [["styled-components", { ssr: true }]],
+  }
 };
 
 module.exports = nextConfig;
